cf9e1a97ee81c4c0c1c4ad6997bf69aa2429efa9
Jenson-08 <jensonpg08@gmail.com>
2024-04-07 00:50:57 -0600

    Update API test

--- cypress/e2e/TC04-API.cy.js
+++ cypress/e2e/TC04-API.cy.js
@@ -1,19 +1,43 @@
 /// <reference types="Cypress" />
 
-describe('API test', () => {
-    it('Specific information from a user', () => {
-      // Realiza una llamada a la API
-      cy.request('GET', 'https://rickandmortyapi.com/api/character/119')
-        .then((response) => {
-          // Verifica que la llamada se haya realizado correctamente
-          expect(response.status).to.eq(200);
-          
-          // Verifica que la respuesta contenga la información esperada
-          expect(response.body).to.have.property('id', 119);
-          expect(response.body).to.have.property('name', 'Evil Rick');
-          expect(response.body).to.have.property('status', 'Dead');
-          expect(response.body).to.have.property('gender', "Male");
+describe('Prueba de API de Artículos', () => {
+  it('Debe obtener la lista de artículos con datos específicos', () => {
+    // Realiza una llamada a la API
+    cy.request({
+      method: 'GET',
+      url: 'https://api.reverb.com/api/articles/recently_featured',
+      headers: {
+        'Accept-Version': '3.0' // API reverb
+      }
+      }).then((response) => {
+        // Status code
+        expect(response.status).to.eq(200);
+
+        // Structure of response
+        expect(response.body).to.have.property('articles').that.is.an('array');
+        expect(response.body.articles).to.have.lengthOf.at.least(1);
+
+        // Verifiy the responses 
+        response.body.articles.forEach((article) => {
+          expect(article).to.have.property('title').that.is.a('string').and.is.not.empty;
+          expect(article).to.have.property('summary').that.is.a('string').and.is.not.empty;
+          expect(article).to.have.property('author_name').that.is.a('string').and.is.equal('Reverb');
+
+          // Verifica que el campo 'author_email' esté presente y sea una cadena vacía
+          expect(article).to.have.property('author_email').that.is.a('string').and.is.empty;
+
+          // Verifica la estructura del campo 'categories'
+          expect(article).to.have.property('categories').that.is.an('array').and.is.not.empty;
+          article.categories.forEach((category) => {
+            expect(category).to.have.property('id').that.is.a('number').and.is.equal(3);
+            expect(category).to.have.property('name').that.is.a('string').and.is.not.empty;
+            expect(category).to.have.property('created_at').that.is.a('string').and.is.equal('2014-11-24T10:59:05-06:00');
+            expect(category).to.have.property('updated_at').that.is.a('string').and.is.equal('2016-12-14T16:21:42-06:00');
+            expect(category).to.have.property('slug').that.is.a('string').and.is.equal('news-and-reviews');
+          });
+
+          // Agrega más expectativas según la estructura de cada artículo en la lista
         });
-    });
+      });
   });
-  
+});
